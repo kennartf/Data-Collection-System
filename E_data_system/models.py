bd94e73237dd262b587c003e1987e1c74e3d423e
kennart <kennartfoundation@gmail.com>
2022-06-26 18:31:59 -0500

    All
    changes made to all the files

--- E_data_system/models.py
+++ E_data_system/models.py
@@ -1,1 +1,36 @@
-# from flask import 
+from datetime import datetime
+from enum import unique
+from sqlalchemy.sql import func
+from flask_admin import Admin
+from flask_login import UserMixin
+from E_data_system import db, app
+from E_data_system import login_manager
+from flask_admin.contrib.sqla import ModelView
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from itsdangerous import TimedJSONWEBSignatureSerializer as Serializer
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name_of_zone = db.Column(db.String(length=100), nullable=False, unique=True)
+    name_of_assembly = db.Column(db.String(length=100), nullable=False, unique=True)
+    head_teacher = db.Column(db.String(length=100), nullable=False, unique=True)
+    total_no_of_children = db.Column(db.String(length=100), nullable=False, unique=True)
+    no_of_children_male = db.Column(db.String(length=50), nullable=False, unique=True)
+    no_of_children_female = db.Column(db.String(length=50), nullable=False, unique=True)
+    total_no_of_Teachers = db.Column(db.String(length=50), nullable=False, unique=True)
+    no_of_teachers_male = db.Column(db.String(length=50), nullable=False, unique=True)
+    no_of_teachers_female = db.Column(db.String(length=50), nullable=False, unique=True)
+    no_of_souls = db.Column(db.String(length=50), nullable=False, unique=True)
+    baptised = db.Column(db.String(length=50), nullable=False, unique=True)
+    offering = db.Column(db.String(length=50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"('{self.name_of_zone}', '{self.name_of_assembly}')"
--- E_data_system/views/routes.py
+++ E_data_system/views/routes.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template, flash, url_for, request, redirect
 
 view = Blueprint('view', __name__)
 
-
+@view.route('/')
 @view.route('/home')
 def home():
     return render_template('home.html')
