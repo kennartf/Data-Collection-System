85ed4e2ab608728e452bc17f47cd5d380638a16a
kenneth <kennartfoundation@gmail.com>
2022-07-17 03:26:51 -0500

    new changes to all the file

--- E_data_system/__init__.py
+++ E_data_system/__init__.py
@@ -29,7 +29,8 @@ mail = Mail(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
-login_manager.login_view = 'authent.login'
+# login_manager.login_view = 'admin_.login'
+login_manager.login_view = 'youth.ylogin'
 login_manager.init_app(app)
 login_manager.login_message_category = 'info'
 
