5110f31bc97f815c3d03f41b34d472d90733aead
kenneth <kennartfoundation@gmail.com>
2022-07-23 12:04:48 -0500

    new changes to all the file

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from E_data_system import app, db
 if __name__ == "__main__":
     db.create_all()
     # db.drop_all()
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', debug=True, port=5000)
     # colors = #243a5e #6093b2 #90dc91 #55b0e2
+    # pip install flask-admin
     
